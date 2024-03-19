3b4126a16d60c1f7188ff6195974a81d2410b0a5
dgochev03 <dobromirgochev@gmail.com>
2024-03-19 14:53:50 +0200

    file operations

--- src/bg/tu_varna/sit/b3/f22621667/oop1_project/Close.java
+++ src/bg/tu_varna/sit/b3/f22621667/oop1_project/Close.java
@@ -1,7 +1,5 @@
 package bg.tu_varna.sit.b3.f22621667.oop1_project;
 
-import java.util.Scanner;
-
 public class Close implements MenuOption{
     @Override
     public void execute() {
--- src/bg/tu_varna/sit/b3/f22621667/oop1_project/Save.java
+++ src/bg/tu_varna/sit/b3/f22621667/oop1_project/Save.java
@@ -1,10 +1,8 @@
 package bg.tu_varna.sit.b3.f22621667.oop1_project;
 
-import java.io.IOException;
-
 public class Save implements MenuOption {
     @Override
-    public void execute() throws IOException {
+    public void execute() {
         Open open=new Open();
         open.save();
         System.out.println("File is saved successfully");
