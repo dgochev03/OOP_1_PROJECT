ece5bb9007a3d53ee0a4982e2e9db4bbf5b269ba
dgochev03 <dobromirgochev@gmail.com>
2024-03-31 14:32:48 +0300

    menu editing

--- src/bg/tu_varna/sit/b3/f22621667/oop1_project/MenuOption.java
+++
@@ -1,7 +0,0 @@
-package bg.tu_varna.sit.b3.f22621667.oop1_project;
-
-import java.io.IOException;
-
-public interface MenuOption {
-    void execute() throws IOException;
-}
---
+++ src/bg/tu_varna/sit/b3/f22621667/oop1_project/contracts/IController.java
@@ -0,0 +1,22 @@
+package bg.tu_varna.sit.b3.f22621667.oop1_project.contracts;
+
+public interface IController {
+
+    int getOptionsCount();
+
+    void open();
+
+    void edit();
+
+    void print();
+
+    void close();
+
+    void save();
+
+    void saveAs();
+
+    void help();
+
+    void exit();
+}
